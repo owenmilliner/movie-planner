8bfa888d8b02f2034f22c1ba0a1f88b454aacd43
owenmilliner <owenmilliner1@gmail.com>
2021-12-13 12:23:39 +0000

    Basic data entry into form created.

--- movie-planner/src/App.js
+++ movie-planner/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import './App.css';
 import InsertForm from './components/InsertForm';
 import MovieList from './components/MovieList';
@@ -5,6 +6,13 @@ import Overview from './components/Overview';
 import Title from './components/Title';
 
 function App() {
+    const [movies, setMovies] = useState({
+        "the_martian_2015": 
+            {title: "The Martian", year: 2015, franchise: "", genre: "Sci-Fi", rating: 5, favourite: true}, 
+        "star_wars:_the_phantom_menace_(episode_i)": 
+            {title:"Star Wars: The Phantom Menace (Episode I)", year: 1999, franchise: "Star Wars", genre: "Sci-Fi", rating: 5, favourite: true}
+        });
+
   return (
     <div className="App">
       <Title />
--- movie-planner/src/components/InsertForm.jsx
+++ movie-planner/src/components/InsertForm.jsx
@@ -1,10 +1,28 @@
+import { useState } from "react";
+
 const InsertForm = () => {
+    const [newMovie, setNewMovie] = useState({title: "", year: 1900, franchise: "", genre: "", rating: 0, favourite: false})
+    
+    const handleOnChange = (property, value) => {
+        const movieObj = {...newMovie};
+
+        movieObj[property] = value;
+
+        setNewMovie(movieObj);
+    }
+
+    const addMovie = (event) => {
+        event.preventDefault();
+        console.log("Movie Submitted (but not actually, I need to add this still).");
+        console.log(newMovie)
+    }
+
     return (
       <section id="InsertForm">
-          <form id="movieForm">
+          <form id="movieForm" onSubmit={addMovie}>
               <div className="formSections">
               <label htmlFor="title">Movie Title:</label>
-              <label htmlFor="date">Year of Release:</label>
+              <label htmlFor="year">Year of Release:</label>
               <label htmlFor="franchise">Franchise:</label>
               <label htmlFor="genre">Genre:</label>
               <label htmlFor="rating">Star Rating:</label>
@@ -12,13 +30,13 @@ const InsertForm = () => {
               </div>
 
               <div className="formSections">
-              <input type="text" id="title"></input>
-              <input type="number" min="1900" max="2021" id="date"></input>
-              <input type="text" id="franchise"></input>
-              <input type="text" id="genre"></input>
-              <input type="number" min="0" max="5" step="0.5" id="rating"></input>
-              <input type="checkbox" id="favourite"></input>
-              <button id="addTasks" type="submit">Add</button>
+              <input type="text" id="title" name="title" value={newMovie.title} onChange={(event) => handleOnChange("title", event.target.value)} required></input>
+              <input type="number" min="1900" max="2021" id="year" name="year" value={newMovie.year} onChange={(event) => handleOnChange("year", event.target.value)} required></input>
+              <input type="text" id="franchise" name="franchise" value={newMovie.franchise} onChange={(event) => handleOnChange("franchise", event.target.value)}></input>
+              <input type="text" id="genre" name="genre" value={newMovie.genre} onChange={(event) => handleOnChange("genre", event.target.value)} required></input>
+              <input type="number" min="0" max="5" step="0.5" id="rating" name="rating" value={newMovie.rating} onChange={(event) => handleOnChange("rating", event.target.value)} required></input>
+              <input type="checkbox" id="favourite" name="favourite" value={newMovie.favourite} onChange={(event) => handleOnChange("favourite", event.target.value)}></input>
+              <button type="submit" id="addMovie">Add</button>
               </div>
 
           </form>
